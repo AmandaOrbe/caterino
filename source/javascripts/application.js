f9932ebcb2830c5ed16e0adbe93542563f476443
(Amanda Orbe) <garayomiriam@gmail.com>
2019-04-25 12:16:54 +0200

    button update

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,17 +1,30 @@
 // = require_tree .
 var unitPrice = Number(document.getElementById("precio").innerText);
 var diners = document.getElementById("comensales");
-
 var priceButton =document.getElementById("button-price");
+var minPrice = unitPrice * 15;
 
+
+var updatePrice = function (event) {
+    var dinersNumber = Number(diners.value);
+    var postre = document.getElementById("postre").checked;
+    var i = postre ? 1 : 0;
+    var calculatedPrice = dinersNumber * (unitPrice + i );
+    console.log(i);
+    if (calculatedPrice >= minPrice ) {
+    priceButton.innerHTML = "Total " + calculatedPrice + " €";
+  } else {
+    priceButton.innerHTML = "Total " + minPrice + " €";
+  }
+};
 
 // console.log(finalPrice);
 
-diners.addEventListener("change", function(event){
+diners.addEventListener("keyup", updatePrice);
+window.addEventListener("click", updatePrice);
+window.addEventListener("DOMContentLoaded", updatePrice);
 
-  var dinersNumber = Number(diners.value);
-  var finalPrice = dinersNumber * unitPrice;
-  console.log(finalPrice);
-  priceButton.innerHTML = finalPrice + " euros"
 
-});
+// postre.addEventListener("onChange", function(event){
+//   if postre
+// }
