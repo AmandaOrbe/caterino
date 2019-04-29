6a7553b574d37d63eb1740e3ff30bc2cca4a4cbf
(Amanda Orbe) <garayomiriam@gmail.com>
2019-04-29 19:36:59 +0200

    number-form

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,13 +1,46 @@
 // = require_tree .
 var unitPrice = Number(document.getElementById("precio").innerText);
+var buttonNext = document.getElementById("button-next");
 var diners = document.getElementById("comensales");
-var priceButton =document.getElementById("button-price");
+var price =document.getElementById("price");
 var postreButton =document.getElementById("button-postre");
 var f1 = document.getElementById("f1");
 var f2 = document.getElementById("f2");
 var f3 = document.getElementById("f3");
 var minPrice = unitPrice * 15;
+var minimo = document.getElementById("minimo")
+var minus = document.getElementById("minus")
+var increase = document.getElementById("increase")
+var decrease = document.getElementById("decrease")
 
+
+var minimoVisible = function( event){
+ if (Number(diners.value) < 15) {
+  minimo.style.opacity = 1;
+ }
+}
+
+var signColor = function(){
+  if (Number(diners.value) > 15){
+      minus.style.fill="#FDD000";
+  } else {
+     minus.style.fill="#BCBABA";
+  }
+}
+
+var increaseDiners  = function (event){
+  var dinersNumber = Number(diners.value);
+  diners.value = Number(document.getElementById("comensales").value) + 1;
+}
+
+var decreaseDiners  = function (event){
+  if (Number(diners.value) > 15){
+    var dinersNumber = Number(diners.value);
+    diners.value = Number(document.getElementById("comensales").value) - 1;
+  } else {
+    minimo.style.opacity = 1;
+  }
+}
 
 var updatePrice = function (event) {
     var dinersNumber = Number(diners.value);
@@ -16,10 +49,12 @@ var updatePrice = function (event) {
     var calculatedPrice = dinersNumber * (unitPrice  );
     // console.log(i);
     if (calculatedPrice >= minPrice ) {
-    priceButton.innerHTML = "Total " + calculatedPrice + " €";
+    price.innerHTML = "Total " + calculatedPrice + " €";
   } else {
-    priceButton.innerHTML = "Total " + minPrice + " €";
+    price.innerHTML = "Total " + minPrice + " €";
   }
+  minimoVisible();
+  signColor();
 };
 
 var stepTwo = function (event) {
@@ -32,14 +67,21 @@ var stepThree = function (event) {
   f3.classList.remove("hidden");
 }
 
+
+
 // console.log(finalPrice);
 
 diners.addEventListener("keyup", updatePrice);
 window.addEventListener("click", updatePrice);
+
+
 window.addEventListener("DOMContentLoaded", updatePrice);
 
-priceButton.addEventListener("click", stepTwo);
+buttonNext.addEventListener("click", stepTwo);
 postreButton.addEventListener("click", stepThree);
+
+increase.addEventListener("click", increaseDiners);
+decrease.addEventListener("click", decreaseDiners);
 // postre.addEventListener("onChange", function(event){
 //   if postre
 // }
