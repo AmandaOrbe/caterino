2bf00577fd4f0670659d20300a4a4ca17460e427
(Amanda Orbe) <garayomiriam@gmail.com>
2019-05-22 21:06:13 +0200

    cards updating themselves for each menu item

---
+++ source/javascripts/custom.js
@@ -0,0 +1,36 @@
+
+//= require jquery
+//= require bootstrap
+
+
+
+
+var numberForms = document.querySelectorAll(".own__number-form");
+
+
+var updateButtonText = function(event, numberForm){
+  button = event.target.closest(".own__instance").querySelector(".button--total a");
+  card = event.target.closest(".own__instance").querySelector(".own__card");
+  item = card.querySelector("h3");
+  price = Number(card.querySelector(".own__price__number").innerText);
+    console.log(button.innerText);
+    button.innerText = "Total " + numberForm.value + "€";
+    if (Number(numberForm.value) > 0) {
+      console.log(price * Number(numberForm.value) )
+      card.style.borderLeft= "7px solid #FDD000"
+      card.querySelector(".own__price").innerText = price * Number(numberForm.value) + "€";
+      card.querySelector(".own__price").style.color= "#FDD000";
+      // document.getElementById("order-items-right").innerHTML('beforeend','<h4>Entrantes</h4>');
+    }
+}
+
+numberForms.forEach(function(numberForm){
+  numberForm.addEventListener("change", function(event){
+    updateButtonText(event, numberForm);
+  });
+  numberForm.addEventListener("keyup", function(event){
+    updateButtonText(event, numberForm);
+  });
+});
+
+
---
+++ source/javascripts/gown.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require bootstrap
+//= require_tree .
+
+var increase = document.getElementById("increase")
+var decrease = document.getElementById("decrease")//= require jquery
+
+
+var unitPrice = document.getElementById("own-form")
