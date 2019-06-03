a1e2456ffb2fd4d5f6493b61dbff4b463459f946
(Amanda Orbe) <garayomiriam@gmail.com>
2019-06-03 18:14:19 +0200

    design for empty

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -7,6 +7,7 @@
 var buttonsTotal = document.querySelectorAll(".button--total");
 var numberForms = document.querySelectorAll(".own__number-form");
 var orderItemsRight = document.getElementById("order-items-right");
+var empty = document.querySelector(".form-right__empty");
 var orderLines = {}
 
 
@@ -17,20 +18,27 @@ var itemsObject= {}
 numberForms.forEach(function(numberForm){
   card = numberForm.closest(".own__instance").querySelector(".own__card")
   itemsObject[numberForm.name.toString()] = {
-    name: numberForm.name,
+    name: numberForm.name.replace(/\s/g, ''),
     unitPrice: Number(card.querySelector(".own__price__number").innerText),
     value: numberForm.value};
 });
 
 
-console.log(itemsObject.croquetas);
+
+
+var isEmpty = function(){
+  if (document.querySelector(".details")){
+    empty.style.display = "none";
+  } else{
+    empty.style.display = "block"
+  }
+
+}
 
 var updatePage = function(event, item){
   card = event.target.closest(".own__instance").querySelector(".own__card");
   value = Number(itemsObject[item].value);
   unitPrice = Number(itemsObject[item].unitPrice);
-
-
    if ( value > 0) {
       card.style.borderLeft= "7px solid #FDD000"
       card.querySelector(".own__price").innerText = unitPrice * value + "€";
@@ -50,6 +58,7 @@ var updatePage = function(event, item){
       line = document.getElementById(itemsObject[item].name + "right");
       orderItemsRight.removeChild(line);
     }
+    isEmpty();
 }
 
 var updateItemsObject = function(event, numberForm){
@@ -58,10 +67,6 @@ var updateItemsObject = function(event, numberForm){
   itemsObject[item].value = numberForm.value ;
 
   updatePage(event, item)
-
-
-
-
 }
 
 
@@ -69,18 +74,19 @@ var updateItemsObject = function(event, numberForm){
 var updateButtonText = function(numberForm){
   item = numberForm.name;
   selectedObject = itemsObject[item];
-  console.log(itemsObject);
-  console.log(numberForm);
-  console.log(itemsObject[item]);
   unitPrice = itemsObject[item].unitPrice;
   button = event.target.closest(".own__instance").querySelector(".button--total a");
   button.innerText = "Total " + numberForm.value * unitPrice + "€";
 
 }
 
+
+
 numberForms.forEach(function(numberForm){
   numberForm.addEventListener("change", function(event){
     updateButtonText(numberForm);
+
+
   });
 
   numberForm.addEventListener("keyup", function(event){
@@ -94,8 +100,7 @@ buttonsTotal.forEach(function(buttonTotal){
   buttonTotal.addEventListener("click", function(event){
     var numberForm = event.target.closest(".modal-content").querySelector(".own__number-form");
     updateItemsObject(event, numberForm);
-   // console.log(numberForm)
-   console.log(itemsObject)
+
   });
 });
 
